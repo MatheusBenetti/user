5a7a850ec6c6cdedb9e1d08d056176ae5cf7c24b
MatheusBenetti <mbbenetti1@gmail.com>
2024-09-04 13:38:09 -0300

    add security classes

---
+++ src/main/java/com/totex/user/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.totex.user.infrastructure.security;
+
+import com.totex.user.infrastructure.entity.UserEntity;
+import com.totex.user.infrastructure.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity   user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
