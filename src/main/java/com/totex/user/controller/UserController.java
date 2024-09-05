aca34b404543b10f3455e0c4ef669c0697317d5c
MatheusBenetti <mbbenetti1@gmail.com>
2024-09-05 14:27:04 -0300

    create user endpoint

---
+++ src/main/java/com/totex/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.totex.user.controller;
+
+import com.totex.user.business.UserService;
+import com.totex.user.business.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.saveUser(userDto));
+    }
+}
